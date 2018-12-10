7ae525dd730e980483c6bbf8557f6f399946fa92
guoliang0_0 <guolang0_0@163.com>
2018-12-10 14:14:08 +0800

    complete switch window function, add recent project list after current opened project option.

--- src/com/lang/project/ProjectListPopupStep.java
+++ src/com/lang/project/ProjectListPopupStep.java
@@ -1,22 +1,21 @@
 package com.lang.project;
 
 
+import com.intellij.ide.RecentProjectsManagerBase;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.openapi.project.ex.ProjectManagerEx;
+import com.intellij.openapi.ui.popup.ListSeparator;
 import com.intellij.openapi.ui.popup.PopupStep;
 import com.intellij.openapi.ui.popup.SpeedSearchFilter;
 import com.intellij.openapi.ui.popup.util.BaseListPopupStep;
 import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.openapi.wm.WindowManager;
-
 import java.awt.Component;
-import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import javax.swing.JFrame;
-
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author guoliang
@@ -26,67 +25,100 @@ import org.jetbrains.annotations.NotNull;
  * @date 7:28 PM 2018/12/5
  */
 public class ProjectListPopupStep extends BaseListPopupStep<Project> {
+	private Project selectedProject;
+	private Project separateProject;
 
-    public ProjectListPopupStep(String title, List<Project> projects) {
-        super(title, projects);
-    }
+	public ProjectListPopupStep(String title, List<Project> projects, Project separateProject) {
+		super(title, projects);
+		this.separateProject = separateProject;
+	}
 
-    @NotNull
-    @Override
-    public String getTextFor(Project value) {
-        return value.getName();
-    }
+	public static void switchSpecifyProjectWindow(Project project) {
+		if (null == project) {
+			return;
+		}
 
-    @Override
-    public SpeedSearchFilter<Project> getSpeedSearchFilter() {
-        return new SpeedSearchFilter<Project>() {
-            @Override
-            public boolean canBeHidden(Project value) {
-                return true;
-            }
+		ProjectManagerEx projectManagerEx = ProjectManagerEx.getInstanceEx();
+		JFrame projectFrame = WindowManager.getInstance().getFrame(project);
+		if (null == projectFrame) {
+			projectManagerEx.openProject(project);
+			projectFrame = WindowManager.getInstance().getFrame(project);
+			if (null == projectFrame) {
+				return;
+			}
+			JFrame finalProjectFrame = projectFrame;
+			IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown(() -> {
+				Component mostRecentFocusOwner = finalProjectFrame.getMostRecentFocusOwner();
+				if (mostRecentFocusOwner != null) {
+					IdeFocusManager.getGlobalInstance().requestFocus(mostRecentFocusOwner, true);
+				}
+			});
+		} else {
+			RecentProjectsManagerBase.getInstanceEx()
+					.doOpenProject(Objects.requireNonNull(project.getBasePath()), project, true);
 
-            @Override
-            public String getIndexedString(Project value) {
-                return value.getName();
-            }
-        };
-    }
+			JFrame finalProjectFrame1 = projectFrame;
+			IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown(() -> {
+				Component mostRecentFocusOwner = finalProjectFrame1.getMostRecentFocusOwner();
+				if (mostRecentFocusOwner != null) {
+					IdeFocusManager.getGlobalInstance().requestFocus(mostRecentFocusOwner, true);
+				}
+			});
+		}
 
+	}
 
-    @Override
-    public boolean isSpeedSearchEnabled() {
-        return true;
-    }
+	@Nullable
+	@Override
+	public Runnable getFinalRunnable() {
+		return () -> switchSpecifyProjectWindow(selectedProject);
+	}
 
-    @Override
-    public PopupStep onChosen(Project project, boolean finalChoice) {
-        if (null == project || project.getBasePath() == null) {
-            return FINAL_CHOICE;
-        }
-        switchSpecifyProjectWindow(project);
-        return FINAL_CHOICE;
-    }
+	@Nullable
+	@Override
+	public ListSeparator getSeparatorAbove(Project value) {
+		if (null != this.separateProject && value == this.separateProject) {
+			return new ListSeparator("Recent Projects");
+		}
 
-    public static void switchSpecifyProjectWindow(Project project) {
-        if (null == project || null == project.getBasePath()) return;
+		if (value == this.getValues().get(0)) {
+			return new ListSeparator("Opened Projects");
+		}
+		return null;
+	}
 
-        ProjectManagerEx projectManagerEx = ProjectManagerEx.getInstanceEx();
-        Project p = null;
-        try {
-            p = projectManagerEx.loadProject(project.getBasePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        JFrame projectFrame = WindowManager.getInstance().getFrame(project);
+	@NotNull
+	@Override
+	public String getTextFor(Project value) {
+		return value.getName();
+	}
 
-        if (null == projectFrame || null == p) return;
+	@Override
+	public SpeedSearchFilter<Project> getSpeedSearchFilter() {
+		return new SpeedSearchFilter<Project>() {
+			@Override
+			public boolean canBeHidden(Project value) {
+				return true;
+			}
 
-        projectManagerEx.openProject(p);
-        IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown(() -> {
-            Component mostRecentFocusOwner = projectFrame.getMostRecentFocusOwner();
-            if (mostRecentFocusOwner != null) {
-                IdeFocusManager.getGlobalInstance().requestFocus(mostRecentFocusOwner, true);
-            }
-        });
-    }
+			@Override
+			public String getIndexedString(Project value) {
+				return value.getName();
+			}
+		};
+	}
+
+	@Override
+	public boolean isSpeedSearchEnabled() {
+		return true;
+	}
+
+	@Override
+	public PopupStep onChosen(Project project, boolean finalChoice) {
+		if (null == project || project.getBasePath() == null) {
+			return FINAL_CHOICE;
+		}
+		this.selectedProject = project;
+		return FINAL_CHOICE;
+	}
 }
--- src/com/lang/project/SwitchProject.java
+++ src/com/lang/project/SwitchProject.java
@@ -1,17 +1,21 @@
 package com.lang.project;
 
+import com.intellij.ide.RecentProjectsManager;
+import com.intellij.ide.ReopenProjectAction;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ex.ProjectManagerEx;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.ui.popup.ListPopup;
-import com.intellij.ui.awt.RelativePoint;
-import java.awt.Point;
+import com.intellij.openapi.util.text.StringUtil;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author guoliang
@@ -23,24 +27,54 @@ import java.util.List;
 public class SwitchProject extends AnAction {
 
 	@Override
-	public void update(AnActionEvent e) {
+	public void update(@NotNull AnActionEvent e) {
 		e.getPresentation().setEnabledAndVisible(ProjectManager.getInstance().getOpenProjects().length > 0);
 	}
 
 	@Override
-	public void actionPerformed(AnActionEvent e) {
+	public void actionPerformed(@NotNull AnActionEvent e) {
 		Project project = e.getData(CommonDataKeys.PROJECT);
-
+		if (null == project) return;
 		Project[] projects = ProjectManager.getInstance().getOpenProjects();
 		List<Project> allProject = new ArrayList<>(Arrays.asList(projects));
 		allProject.remove(project);
+
+		List<Project> recentProjects = new ArrayList<>();
+		AnAction[] actions = RecentProjectsManager.getInstance().getRecentProjectsActions(false);
+		for (AnAction action : actions) {
+			if (!(action instanceof ReopenProjectAction)) {
+				continue;
+			}
+			ReopenProjectAction reopenProjectAction = (ReopenProjectAction) action;
+			if (StringUtil.equals(reopenProjectAction.getProjectPath(), project.getBasePath())) {
+				continue;
+			}
+
+			boolean contained = false;
+			for (Project openedProject : allProject) {
+				if (StringUtil.equals(reopenProjectAction.getProjectPath(), openedProject.getBasePath())) {
+					contained = true;
+					break;
+				}
+			}
+			try {
+				if (!contained) {
+					recentProjects.add(ProjectManagerEx.getInstanceEx().loadProject(reopenProjectAction.getProjectPath()));
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+		allProject.addAll(recentProjects);
 		if (allProject.size() == 1) {
 			ProjectListPopupStep.switchSpecifyProjectWindow(allProject.get(0));
 			return;
 		}
 		ListPopup listPopup = JBPopupFactory.getInstance()
-				.createListPopup(new ProjectListPopupStep("", allProject));
+				.createListPopup(new ProjectListPopupStep("", allProject, recentProjects.isEmpty() ? null : recentProjects.get(0)));
 		listPopup.showInFocusCenter();
 	}
+
 
 }
